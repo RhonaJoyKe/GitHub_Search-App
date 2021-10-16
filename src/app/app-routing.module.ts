1dd3e3fd8c6e4808493d7c913a5506e4a1539c62
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-16 11:14:28 +0300

    adds the router functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { RepositoryComponent } from './repository/repository.component';
 
 const routes: Routes = [
   { path: 'user', component: UserComponent},
   { path: 'repository', component: RepositoryComponent},
+  { path: 'home', component: HomeComponent},
+
 ];
 
 @NgModule({
