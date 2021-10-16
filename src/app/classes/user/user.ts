e67c0be810fa05f8d225e2f01e6dc03e901c3c6c
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-16 14:30:57 +0300

    Adds constructor parameters

--- src/app/classes/user/user.ts
+++ src/app/classes/user/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    constructor(
+    public name: string,
+    public username: string,
+    public avatarurl: string,
+    public location: string,
+    public bio: string,
+     public followers: number,
+    public following: number,
+  
+    ){}   
 }
