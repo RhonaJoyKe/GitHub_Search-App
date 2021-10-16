11f6e3b209e78b8eab1241b63edde36994adf2e2
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-16 16:39:42 +0300

    initializes user and repo details constructor

--- src/app/services/usrepodata/usrepodata.service.ts
+++ src/app/services/usrepodata/usrepodata.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/classes/user/user';
+import { Repos } from 'src/app/classes/repository/repos';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsrepodataService {
+  getuserdetails:User;
+  getrepodetails:Repos;
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.getuserdetails=new User(
+
+      "","","","","",0,0
+    )
+    this.getrepodetails=new Repos(
+
+      "","","","",
+    )
+   
+   }
 }
