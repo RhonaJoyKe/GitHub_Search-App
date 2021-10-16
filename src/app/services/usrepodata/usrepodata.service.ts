6901a459752bd36965c48a595b96f09b3300d529
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-16 21:19:54 +0300

    Adds User Promise

--- src/app/services/usrepodata/usrepodata.service.ts
+++ src/app/services/usrepodata/usrepodata.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { User } from 'src/app/classes/user/user';
 import { Repos } from 'src/app/classes/repository/repos';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -21,4 +22,40 @@ export class UsrepodataService {
     )
    
    }
+   fetchUserRequest(ghubUsername: string){
+     interface ApiUserResponse{
+       name: string,
+       username: string,
+       avatarurl: string,
+       location: string,
+       bio: string,
+        followers: number,
+       following: number,
+    
+
+     }
+     let userPromise = new Promise<void>((resolve, reject) =>
+      this.http
+        .get<ApiUserResponse>(
+          environment.apiKey +
+            '/' +
+            ghubUsername +
+            '?access_token=' +
+            environment.apiKey
+        )
+        .toPromise()
+        .then(
+          (response) => {
+            this.getuserdetails = response;
+            resolve();
+          },
+          (error) => {
+            reject(error);
+            console.log(error);
+          }
+        )
+    );
+    return userPromise;
+  }
+
 }
